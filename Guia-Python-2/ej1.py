69ae5e04b6ea86ec224c8d6188e166d69af35aeb
Carlos Vercellone <carlosvercellone21@gmail.com>
2024-03-15 11:32:01 -0300

    Agregego el ejercicio 1

---
+++ Guia-Python-2/ej1.py
@@ -0,0 +1,17 @@
+class Persona:
+
+    def inicializar(self,name):
+        self.nombre= name 
+
+    def imprimir(self):
+        print("Nombre de la persona:",self.nombre)
+
+
+
+persona1 = Persona()
+persona1.inicializar("Juan")
+persona1.imprimir()
+
+persona2 = Persona()
+persona2.inicializar("Kevin")
+persona2.imprimir()
